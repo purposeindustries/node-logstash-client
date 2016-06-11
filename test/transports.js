0fd1ed081676af685a7b8fb11b6d90a852cf0f9d
Rene Weber <reweber@ebay.com>
2016-06-11 18:40:25 +0100

    Add option to restrict queue size

--- test/transports.js
+++ test/transports.js
@@ -9,6 +9,25 @@ test('Offline support for transport', function(t) {
   t.end();
 });
 
+test('Maximum queue size when offline', function(t) {
+  var transport = new (Transports.Transport)({maxQueueSize: 1});
+  transport.send('foo');
+  transport.send('foo1');
+  transport.send('foo2');
+  t.equal(transport.queue.length, 1, 'should not exceed maxQueueSize');
+  t.equal(transport.queue[0].message, 'foo2', 'should store message');
+  t.end();
+});
+
+test('Unlimited queue size when offline', function(t) {
+  var transport = new (Transports.Transport)();
+  for(var i = 0; i < 10000; i++) {
+    transport.send('foo');
+  }
+  t.equal(transport.queue.length, 10000, 'should include all messages');
+  t.end();
+});
+
 test('Send messages if transport connected', function(t) {
   var transport = new (Transports.Transport)();
   transport.connected = true;
